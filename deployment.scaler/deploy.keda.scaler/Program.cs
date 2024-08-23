8ceb2a127f04dbff5b31750c107456102f24598a
VenkateshSrini <venkateshsrini3@gmail.com>
2024-08-23 10:05:02 +0530

    updated dockerfile, created yaml and altered the prohra,.cs to add gRPC sepcific code

--- deployment.scaler/deploy.keda.scaler/Program.cs
+++ deployment.scaler/deploy.keda.scaler/Program.cs
@@ -2,6 +2,14 @@ using deploy.keda.scaler.Extension;
 using deploy.keda.scaler.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(webOptions =>
+{
+    var gRpcPort = builder.Configuration.GetValue<int>("GrpcPort");
+    webOptions.ListenAnyIP(gRpcPort, kestrelOptions =>
+    {
+        kestrelOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
+    });
+});
 
 // Add services to the container.
 builder.Services.AddGrpc();
@@ -11,6 +19,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<KedaScalerService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
