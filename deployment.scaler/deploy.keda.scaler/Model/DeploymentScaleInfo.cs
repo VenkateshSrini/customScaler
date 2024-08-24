f62e5076fb3384464b1b1f8234024637cae99f55
VenkateshSrini <venkateshsrini3@gmail.com>
2024-08-24 18:39:21 +0530

    updated the code for control-plane

--- deployment.scaler/deploy.keda.scaler/Model/DeploymentScaleInfo.cs
+++ deployment.scaler/deploy.keda.scaler/Model/DeploymentScaleInfo.cs
@@ -1,7 +1,13 @@
-﻿namespace deploy.keda.scaler.Model
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace deploy.keda.scaler.Model
 {
     public class DeploymentScaleInfo
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
         public string Name { get; set; }
         public string Namespace { get; set; }
         public bool IsScalingActive { get; set; }
--- deployment.scaler/scaler.control.plain/Controllers/ControlPlaneController.cs
+++ deployment.scaler/scaler.control.plain/Controllers/ControlPlaneController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using scaler.control.plain.Model;
 using scaler.control.plain.Repo;
 
 namespace scaler.control.plain.Controllers
@@ -15,6 +16,24 @@ namespace scaler.control.plain.Controllers
         {
             _controlPlaneRepo = controlPlaneRepo;
             _logger = logger;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] DeploymentScaleInfo request)
+        {
+            var response = await _controlPlaneRepo.UpsertDeploymentInfoAsync(request);
+            return Ok(response);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get([FromRoute] string ns, [FromRoute] string name)
+        {
+            var response = await _controlPlaneRepo.GetDeploymentScaleInfoAsync(ns, name);
+            return Ok(response);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromRoute] string ns, [FromRoute] string name)
+        {
+            var response = await _controlPlaneRepo.DeleteDeploymentInfoAsync(ns, name);
+            return Ok(response);
         }
     }
 }
