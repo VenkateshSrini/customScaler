5ac452579077fbb02ba706434cbb0174aa2753c8
VenkateshSrini <venkateshsrini3@gmail.com>
2024-08-25 09:16:01 +0530

    Added test deployment

---
+++ deployment.scaler/test-scaled-object/Controllers/DisplayController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace test_scaled_object.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DisplayController : ControllerBase
+    {
+        private readonly ILogger<DisplayController> _logger;
+        private readonly IConfiguration _config;
+        public DisplayController(ILogger<DisplayController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+        public ActionResult Get() {
+            var podName = _config["podName"];
+            return Ok($"Hello from {podName}");
+        }
+    }
+}
